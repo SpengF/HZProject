3fc940b4ca0ca4c3e97c81fb68ccd3cbddaaa676
280431 <27324249@qq.com>
2020-01-15 13:21:03 +0800

    注释掉调试工具

--- newBestDay/pages/authorization/authorization.js
+++ newBestDay/pages/authorization/authorization.js
@@ -16,9 +16,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setEnableDebug({
-      enableDebug: true
-    })
+    // wx.setEnableDebug({
+    //   enableDebug: true
+    // })调试工具
   },
   bindgetuserinfo(e){
     if(e.detail.errMsg==="getUserInfo:ok"&&app.globalData.openid){
